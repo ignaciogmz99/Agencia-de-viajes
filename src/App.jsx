839e7aa33173485d961d6db3c73bb1afd4d8fc70
ignaciogmz99 <ignaciogmz99@gmail.com>
2025-12-12 12:06:52 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar.jsx'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <p>Bienvenido a nuestra página</p>
+    </div>
+  )
+}
+
+export default App
